dc26c67797b0d8a9cffb8c073ce5612a91786d55
Daniel Epstein <epstein@ics.uci.edu>
2019-02-12 20:43:41 -0800

    Initial import, with a sample webm and png recording

---
+++ routes/index.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var puppeteer = require('puppeteer');
+var WebMWriter = require('webm-writer');
+
+var testAnimFile = "animations/testanim.webm";
+var testStillFile = "animations/teststill.png";
+
+async function recordFile(url, filename) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url, {waitUntil: 'networkidle2'});
+
+    var element = await page.waitForSelector("#test");
+    const properties = await element.getProperty("textContent");
+    const property = await properties.jsonValue();
+    await browser.close();
+    var file = Buffer.from(property, 'base64');
+    fs.writeFileSync(filename, file);
+    return file;
+}
+
+async function recordTestAnimation (server, filename) {
+    return await recordFile('http://' + server + '/testanim.html', filename);
+};
+
+async function recordTestStill (server, filename) {
+    return await recordFile('http://' + server + '/teststill.html', filename);
+};
+
+router.get('*', function(req, res, next) {
+	next();
+});
+
+router.get('/testanim', async function(req, res, next) {
+    var buffer;
+    if(fs.existsSync(testAnimFile)) {
+        buffer = fs.readFileSync(testAnimFile);
+    } else {
+        buffer = await recordTestAnimation(req.headers.host, testAnimFile);
+    }
+	res.set('Content-Type', 'video/webm');
+    res.send(buffer);
+});
+
+router.get('/teststill', async function(req, res, next) {
+    var buffer;
+    if(fs.existsSync(testStillFile)) {
+        buffer = fs.readFileSync(testStillFile);
+    } else {
+        buffer = await recordTestStill(req.headers.host, testStillFile);
+    }
+    res.set('Content-Type', 'img/png');
+    res.send(buffer);
+});
+
+module.exports = router;
