1a9201396a1461c15de5c646fcde43c5f7b2a34e
dennis wang <splash9245@gmail.com>
2021-04-17 14:01:34 -0500

    add logger to test parameters sent

--- routes/index.js
+++ routes/index.js
@@ -155,6 +155,8 @@ router.get('/generic', async function(req, res, next) {
 // page for sticker sharing
 router.get('/sticker/:link', function(req, res, next) {
 
+  console.log('variables: ' + `type=${type}, variation=${variation}, value=${value}, unit=${unit}, option=${option}, color=${color}, goal=${goal}, time=${time}`);
+
   const type = req.query.type;                                    // ["plain", "chartjunk", "analogy"]
   const variation = req.query.variation;                          // [1, 2, 3, ...]
   const value = req.query.value;                                  // int[1...]
@@ -165,7 +167,7 @@ router.get('/sticker/:link', function(req, res, next) {
                                                                 // default to value param, if empty
   const time = req.query.time ? req.query.time : "false";
   var stickerLink = req.protocol + '://' + req.headers.host + '/' + req.params.link + '/?' + `type=${type}&variation=${variation}&value=${value}&unit=${unit}&option=${option}&color=${color}&goal=${goal}&time=${time}`;
-  console.log(stickerLink);
+  console.log("stickerLink: " + stickerLink);
   res.set('Content-Type', 'text/html');
   res.send("<html><head><meta content=\"Snapchat\" property=\"og:site_name\" /><meta content=\"Share your sticker!\" property=\"og:title\" /><meta content=\"" + stickerLink + "\" property=\"snapchat:sticker\" /></head><body><img src=\"" + stickerLink + "\"></body></html>");
 });
